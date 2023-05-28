17c7dc5cbd9f01a943ed567aaf32a839f9a19ad2
Xaviar Boykins <119615611+xaviarboykins@users.noreply.github.com>
2023-05-28 10:46:24 -0700

    Add files via upload

---
+++ DataAccessLayer/ProductDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StockSense.Model;
+
+namespace StockSense.DbContextFolder
+{
+    public class ProductDbContext : DbContext
+    {
+        public ProductDbContext(DbContextOptions<ProductDbContext> options) : base(options)
+        {   
+        }
+        public DbSet<Product> Products { get; set; }
+       
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("your_connection_string_here");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasKey(c => c.CategoryId);
+            modelBuilder.Entity<Supplier>().HasKey(s => s.SupplierId);
+            // other entity configurations
+        }
+    }
+}
